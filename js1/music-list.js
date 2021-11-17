f13fe67ea2a6a6faef2e1aab2122e8c62df15f38
Krunal Bhimajiyani <84042512+KrunalKB@users.noreply.github.com>
2021-11-17 19:13:25 +0530

    added

---
+++ js1/music-list.js
@@ -0,0 +1,78 @@
+
+
+let allMusic = [
+  {
+    name: "Bilionera",
+    artist: "Otilia",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Memories",
+    artist: "Martoon 5",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hold On",
+    artist: "Justin Bieber",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Love Me Like You Do ",
+    artist: "Ellie Gouldind",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Faded ",
+    artist: "Alan Walker",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Dance Monkey ",
+    artist: "Tones And I",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Friends ",
+    artist: "Marshmello",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "See You Again ",
+    artist: "Wiz Khalifa",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Thunder ",
+    artist: "Imagine Dragons",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Peaches ",
+    artist: "Justin Bieber",
+    img: "music-12",
+    src: "music-12"
+  },
+
+
+];
